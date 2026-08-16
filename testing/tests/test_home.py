8ab095b09728a5e3ad792644bd0c5049c5755bb7
Keaton Nordby <keatonnordby4@gmail.com>
2026-08-16 13:13:00 -0500

    test: add initial homepage UI and filter tests

--- testing/tests/test_home.py
+++ testing/tests/test_home.py
@@ -21,20 +21,51 @@ def test_homepage_search(app: Page):
 
     app.get_by_role("button", name="Search").click()
     expect(app.get_by_text("Mechanical Keyboard")).to_be_visible()
-    
-    
 
 
-def test_homepage_filters(app: Page):
-    """Verify users can filter and sort products from the homepage."""
-    pass
+def test_homepage_category_filter(app: Page):
+    """Verify users can filter products by category."""
+    category_filter = app.get_by_role("combobox", name="Category: ")
+    category_filter.select_option("Accessories")
+
+    products = app.locator(".product-card")
+    expect(products).to_have_count(2)
+
+    expect(app.get_by_text("Monitor Stand")).to_be_visible()
+
+
+def test_homepage_sort_price_high_to_low(app: Page):
+    """Verify users can sort products from highest to lowest price"""
+    sort_by_filter = app.get_by_role("combobox", name="Sort by: ")
+    sort_by_filter.select_option("price-high")
+
+    products = app.locator(".product-card")
+    expect(products).to_have_count(6)
+    expect(products.first.locator("h3")).to_have_text("Mechanical Keyboard")
+    expect(products.last.locator("h3")).to_have_text("Mouse Pad XL")
+
+
+def test_homepage_sort_price_low_to_high(app: Page):
+    """Verify users can sort products from lowest to highest price"""
+    sort_by_filter = app.get_by_role("combobox", name="Sort by: ")
+    sort_by_filter.select_option("price-low")
+
+    products = app.locator(".product-card")
+    expect(products).to_have_count(6)
+    expect(products.first.locator("h3")).to_have_text("Mouse Pad XL")
+    expect(products.last.locator("h3")).to_have_text("Mechanical Keyboard")
+
+
+def test_homepage_filter_price_drag(app: Page):
+    """Verify users can filter products by price selection"""
+    price_filter = app.get_by_role("slider", name="Max Price: $200")
+    price_filter.fill("50")
+
+    products = app.locator(".product-card")
+    expect(products).to_have_count(2)
 
 
 def test_homepage_products(app: Page):
     """Verify products are displayed correctly on the homepage."""
-    pass
-
-
-def test_homepage_add_to_cart(app: Page):
-    """Verify users can add a product to their shopping cart."""
-    pass
+    products = app.locator(".product-card")
+    expect(products).to_have_count(6)
